f36b7f8c0c807040e6600d650acb4e4207b6167c
Michael Francisco <michaelfrancisco64@gmail.com>
2019-10-01 18:10:58 +0800

    Format views

---
+++ PS4-MIS v2.0/ViewModels/Blotter/BlotterViewModel.cs
@@ -0,0 +1,199 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS4_MIS_v2._0.ViewModels.Blotter
+{
+    class BlotterViewModel
+    {
+        private DataTable _basecriminalRecordsGridItemSource;
+        private string _typeofincident;
+        private string _criminalID;
+        private object _criminalRecordDatagridSelectedItem;
+        private DataTable _criminalRecordsGridItemSource;
+        private string _firstname;
+        private string _lastname;
+        private string _location;
+        private int _selectedCriminalID;
+        IWindowManager windowManager = new WindowManager();
+        public string typeofincident
+        {
+            get { return _typeofincident; }
+            set
+            {
+                _typeofincident = value;
+                DataView dv = new DataView(_basecriminalRecordsGridItemSource);
+                dv.RowFilter = query();
+                _criminalRecordsGridItemSource = dv.ToTable();
+                NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            }
+        }
+
+        public string criminalID
+        {
+            get { return _criminalID; }
+            set
+            {
+                _criminalID = value;
+                DataView dv = new DataView(_basecriminalRecordsGridItemSource);
+                dv.RowFilter = query();
+                _criminalRecordsGridItemSource = dv.ToTable();
+                NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            }
+        }
+
+        public object criminalRecordDatagridSelectedItem
+        {
+            get { return _criminalRecordDatagridSelectedItem; }
+            set { _criminalRecordDatagridSelectedItem = value; }
+        }
+
+        public DataTable criminalRecordsGridItemSource
+        {
+            get { return _criminalRecordsGridItemSource; }
+            set { _criminalRecordsGridItemSource = value; }
+        }
+
+        public string firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                _firstname = value;
+                DataView dv = new DataView(_basecriminalRecordsGridItemSource);
+                dv.RowFilter = query();
+                _criminalRecordsGridItemSource = dv.ToTable();
+                NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            }
+        }
+
+        public string lastname
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                DataView dv = new DataView(_basecriminalRecordsGridItemSource);
+                dv.RowFilter = query();
+                _criminalRecordsGridItemSource = dv.ToTable();
+                NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            }
+        }
+
+        public string location
+        {
+            get { return _location; }
+            set
+            {
+                _location = value;
+                DataView dv = new DataView(_basecriminalRecordsGridItemSource);
+                dv.RowFilter = query();
+                _criminalRecordsGridItemSource = dv.ToTable();
+                NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            }
+        }
+
+        public void addCriminalRecordButton()
+        {
+            windowManager.ShowWindow(new AddCriminalRecordViewModel(), null, null);
+        }
+
+        public string query()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (_firstname != null && _firstname != string.Empty)
+            {
+                sb.Append("First_Name like '%" + _firstname.Trim() + "%'");
+            }
+
+            if (_lastname != null && _lastname != string.Empty)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(" and ");
+                }
+
+                sb.Append("Last_Name like '%" + _lastname.Trim() + "%'");
+            }
+
+            if (_typeofincident != null && _typeofincident != string.Empty)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(" and ");
+                }
+
+                sb.Append("Crime like '%" + _typeofincident.Trim() + "%'");
+            }
+
+            if (_criminalID != null && _criminalID != string.Empty)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(" and ");
+                }
+
+                sb.Append("Criminal_ID like '%" + _criminalID.Trim() + "%'");
+            }
+
+            if (_location != null && _location != string.Empty)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(" and ");
+                }
+
+                sb.Append("Place_of_Arrest like '%" + _location.Trim() + "%'");
+            }
+            return sb.ToString();
+        }
+
+        public void refreshButton()
+        {
+            _criminalRecordsGridItemSource = connection.dbTable("SELECT Criminal_ID, First_Name, Middle_Name, Last_Name, Sex, Birthdate, Age, Birthplace, Address, Crime, Place_of_Arrest, Arresting_Officer, Date_of_Arrest, Eye_Color, Hair_Color, Remarks FROM `ps4`.`criminalrecords`;");
+            _basecriminalRecordsGridItemSource = _criminalRecordsGridItemSource;
+            NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+        }
+
+        public void resetButton()
+        {
+            _firstname = string.Empty;
+            _lastname = string.Empty;
+            _criminalID = string.Empty;
+            _typeofincident = string.Empty;
+            _location = string.Empty;
+            _criminalRecordsGridItemSource = connection.dbTable("SELECT Criminal_ID, First_Name, Middle_Name, Last_Name, Sex, Birthdate, Age, Birthplace, Address, Crime, Place_of_Arrest, Arresting_Officer, Date_of_Arrest, Eye_Color, Hair_Color, Remarks FROM `ps4`.`criminalrecords`;");
+            _basecriminalRecordsGridItemSource = _criminalRecordsGridItemSource;
+            NotifyOfPropertyChange(() => firstname);
+            NotifyOfPropertyChange(() => lastname);
+            NotifyOfPropertyChange(() => criminalID);
+            NotifyOfPropertyChange(() => crime);
+            NotifyOfPropertyChange(() => location);
+            NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+        }
+
+        public void showCriminalRecord()
+        {
+            DataRowView dataRowView = (DataRowView)_criminalRecordDatagridSelectedItem;
+            _selectedCriminalID = Convert.ToInt32(dataRowView.Row[0]);
+            windowManager.ShowWindow(new EditCriminalRecordViewModel(_selectedCriminalID), null, null);
+        }
+
+        protected override void OnActivate()
+        {
+            _criminalRecordsGridItemSource = connection.dbTable("SELECT Criminal_ID, First_Name, Middle_Name, Last_Name, Sex, Birthdate, Age, Birthplace, Address, Crime, Place_of_Arrest, Arresting_Officer, Date_of_Arrest, Eye_Color, Hair_Color, Remarks FROM `ps4`.`criminalrecords`;");
+            _basecriminalRecordsGridItemSource = _criminalRecordsGridItemSource;
+            NotifyOfPropertyChange(() => criminalRecordsGridItemSource);
+            base.OnActivate();
+        }
+
+        private void NotifyOfPropertyChange(Func<DataTable> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PS4-MIS v2.0/ViewModels/MainViewModel.cs
+++ PS4-MIS v2.0/ViewModels/MainViewModel.cs
@@ -1,30 +1,40 @@
 ﻿using Caliburn.Micro;
+using PS4_MIS_v2._0.ViewModels.Blotter;
+using PS4_MIS_v2._0.ViewModels.Bookings;
 using System.Windows.Media;
 
 namespace PS4_MIS_v2._0.ViewModels
 {
     internal class MainViewModel : Conductor<object>
     {
+        private Brush _blotterBrush;
+        private Brush _bookingsBrush;
         private Brush _criminalRecordsBrush;
-        private Brush _policeReportsBrush;
         private Brush _employeerecordsBrush;
         private Brush _inventoryBrush;
         private Brush _messagesBrush;
         private Brush _payrollBrush;
+        private Brush _policeReportsBrush;
         private Brush _stolenvehicleBrush;
         private Brush _visitorlogbookbrush;
         private IWindowManager windowManager = new WindowManager();
+
+        public Brush blotterBrush
+        {
+            get { return _blotterBrush; }
+            set { _blotterBrush = value; }
+        }
+
+        public Brush bookingsBrush
+        {
+            get { return _bookingsBrush; }
+            set { _bookingsBrush = value; }
+        }
 
         public Brush criminalRecordsBrush
         {
             get { return _criminalRecordsBrush; }
             set { _criminalRecordsBrush = value; }
-        }
-
-        public Brush policeReportsBrush
-        {
-            get { return _policeReportsBrush; }
-            set { _policeReportsBrush = value; }
         }
 
         public Brush employeerecordsBrush
@@ -50,48 +60,38 @@ namespace PS4_MIS_v2._0.ViewModels
             get { return _payrollBrush; }
             set { _payrollBrush = value; }
         }
+
+        public Brush policeReportsBrush
+        {
+            get { return _policeReportsBrush; }
+            set { _policeReportsBrush = value; }
+        }
         public Brush stolenvehicleBrush
         {
             get { return _stolenvehicleBrush; }
             set { _stolenvehicleBrush = value; }
         }
-
         public Brush visitorlogbookbrush
         {
             get { return _visitorlogbookbrush; }
             set { _visitorlogbookbrush = value; }
         }
 
-        public void criminalRecordsButton()
+        public void blotterButton()
         {
-            ActivateItem(new CriminalRecordsViewModel());
+            ActivateItem(new BlotterViewModel());
             clearColors();
-            _criminalRecordsBrush = new SolidColorBrush(Colors.DarkBlue);
-            NotifyOfPropertyChange(() => criminalRecordsBrush);
+            _blotterBrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => blotterBrush);
         }
 
-        public void messagesButton()
+        public void bookingsButton()
         {
+            ActivateItem(new BookingsViewModel());
             clearColors();
-            _messagesBrush = new SolidColorBrush(Colors.DarkBlue);
-            NotifyOfPropertyChange(() => messagesBrush);
+            _bookingsBrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => bookingsBrush);
         }
-
-        public void visitorLogbookButton()
-        {
-            clearColors();
-            _visitorlogbookbrush = new SolidColorBrush(Colors.DarkBlue);
-            NotifyOfPropertyChange(() => visitorlogbookbrush);
-        }
-
-        public void policeReportsButton()
-        {
-            ActivateItem(new PoliceReportsViewModel());
-            clearColors();
-            _policeReportsBrush = new SolidColorBrush(Colors.DarkBlue);
-            NotifyOfPropertyChange(() => policeReportsBrush);
-        }
-
         public void clearColors()
         {
             _criminalRecordsBrush = null;
@@ -111,6 +111,15 @@ namespace PS4_MIS_v2._0.ViewModels
             _policeReportsBrush = null;
             NotifyOfPropertyChange(() => policeReportsBrush);
         }
+
+        public void criminalRecordsButton()
+        {
+            ActivateItem(new CriminalRecordsViewModel());
+            clearColors();
+            _criminalRecordsBrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => criminalRecordsBrush);
+        }
+
         public void employeeRecordsButton()
         {
             ActivateItem(new EmployeeRecordsViewModel());
@@ -133,6 +142,13 @@ namespace PS4_MIS_v2._0.ViewModels
             TryClose();
         }
 
+        public void messagesButton()
+        {
+            clearColors();
+            _messagesBrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => messagesBrush);
+        }
+
         public void payrollButton()
         {
             ActivateItem(new PayrollViewModel());
@@ -140,12 +156,28 @@ namespace PS4_MIS_v2._0.ViewModels
             _payrollBrush = new SolidColorBrush(Colors.DarkBlue);
             NotifyOfPropertyChange(() => payrollBrush);
         }
+
+        public void policeReportsButton()
+        {
+            ActivateItem(new PoliceReportsViewModel());
+            clearColors();
+            _policeReportsBrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => policeReportsBrush);
+        }
+
         public void stolenVehiclesButton()
         {
             ActivateItem(new StolenVehiclesViewModel());
             clearColors();
             _stolenvehicleBrush = new SolidColorBrush(Colors.DarkBlue);
             NotifyOfPropertyChange(() => stolenvehicleBrush);
+        }
+
+        public void visitorLogbookButton()
+        {
+            clearColors();
+            _visitorlogbookbrush = new SolidColorBrush(Colors.DarkBlue);
+            NotifyOfPropertyChange(() => visitorlogbookbrush);
         }
     }
 }
