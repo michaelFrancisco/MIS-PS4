ea07bc82028b6e2c47722a08fc6f78e8c6f8bdcb
Michael Francisco <michaelfrancisco64@gmail.com>
2019-10-03 19:51:12 +0800

    UI Update

---
+++ PS4-MIS v2.0/Views/Messages/MessagesView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PS4_MIS_v2._0.Views.Messages
+{
+    /// <summary>
+    /// Interaction logic for MessagesView.xaml
+    /// </summary>
+    public partial class MessagesView : UserControl
+    {
+        public MessagesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
